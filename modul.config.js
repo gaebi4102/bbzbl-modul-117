ecbf2b29b30404c71c6ac7e46ef1a15c68886775
gaebi4102 <129516516+gaebi4102@users.noreply.github.com>
2023-06-23 11:37:21 +0200

    let's get started

---
+++ modul.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  title: "Modul 117",
+  tagline: "Informatik- und Netzinfrastruktur für ein kleines Unternehmen realisieren",
+  repoName: "bbzbl-modul-117",
+  url: 'https://gaebi4102.github.io',
+  organizationName: "gaebi4102", 
+}
